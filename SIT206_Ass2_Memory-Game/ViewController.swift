3068d5a01131a96076c52064bbf5b4ac290b14fa
user126206 <user126206@103-246-99-70.moboware.com>
2017-05-20 20:05:42 +1000

    Initial Commit

---
+++ SIT206_Ass2_Memory-Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIT206_Ass2_Memory-Game
+//
+//  Created by user126206 on 5/20/17.
+//  Copyright © 2017 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
